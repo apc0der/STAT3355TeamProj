d5f8d5d2833e5718af42423245f05757f1c7862d
Subre Moktar <109233694+Subre-Moktar@users.noreply.github.com>
2022-10-22 18:39:33 -0500

    Add files via upload

--- read covid data.R
+++ read covid data.R
@@ -1,8 +1,10 @@
 
+library(usmap)
+library(maps)
 library(reshape)
 library(ggplot2)
-library(dplyr)
-library(readr)
+library(tidyverse)
+library(cowplot)
 df <- list.files(path = "C:/Development/R Program/Data Set Saved Here/COVID-19-master/csse_covid_19_data/csse_covid_19_daily_reports_us", full.names = TRUE)%>%
   lapply(read_csv) %>%
   bind_rows
@@ -14,19 +16,22 @@ table(df$Mortality_Rate)
 
 str(df$Date)
 
-df = filter(df, Date <= "2022-04-12")
+df <- filter(df, Date <= "2022-04-12")
 
 
-df <- select(df, -c(Last_Update, Lat, Long_, Country_Region))
-temp <- select(df, c(Date, Confirmed, Province_State))
+df <- select(df, -c(Last_Update, Country_Region))
 
 colSums(is.na(df))
 
 table(df$FIPS)
 
-ggplot(data = df, mapping = aes(x = Date, y = Confirmed, color = Province_State)) +
+ggplot(data = df, mapping = aes(x = Date, y = Confirmed, color = Province_State,)) +
   geom_point()
 
+ggplot(data = df, mapping = aes(x = Date, y = Deaths, color = Province_State,)) +
+  geom_point()
+
+plot_grid(confirmed, deaths)
 
 
 ggplot(data = df, mapping = aes(x = Date, y = Confirmed)) +
@@ -38,3 +43,20 @@ ggplot(data = df, mapping = aes(x = Date, y = Confirmed)) +
 #  facet_wrap(~(Province_State), scales = "free", nrow = 5) +
 #  theme(axis.text.x = element_blank(), axis.text.y = element_blank())
 
+plot_usmap()
+
+MainStates <- map_data("state")
+
+df <- df %>% rename(region = Province_State)
+df2 <- filter(df, Date == "2022-04-12")
+
+df2$region <- tolower(df2$region)
+
+work<-left_join(MainStates,df2,by = "region")
+
+
+ggplot(work,mapping =  aes(x = long, y =lat, group= group, fill = Deaths)) +
+  geom_polygon() +
+  coord_quickmap()
+
+  
